720179fc441041e1064bc2db9cac5ce2131fcf5c
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-06-07 10:50:57 +0100

    rainbows! and seb!

--- www/index.php
+++ www/index.php
@@ -5,9 +5,15 @@
         <meta http-equiv="Content-Type"   content="text/html; charset=utf-8" />
         <meta name="viewport"             content="width=device-width, initial-scale=1" />
         <title>Rainbow Squadron</title>
+        <script src="jquery.js"></script>
+        <script src="jquery-ui.js"></script>
+        <script src="rbw.js"></script>
+        <link type="text/css" rel="stylesheet" href="main.css"/>
+        <link rel="icon" href="favicon.png">
     </head>
     <body>
-        Welcome to the Rainbow Squadron!
-        <script> window.location.replace("http://steamcommunity.com/groups/Rainbow-Squadron"); </script>
+        <div id="main">
+            <h1>Welcome to the Rainbow Squadron!</h1>
+        </div>
     </body>
 </html>
