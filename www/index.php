3c1580ff80325daa4afe9016e2f3d6d8bdcae10e
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-06-07 11:16:53 +0100

    added SEO basically

--- www/index.php
+++ www/index.php
@@ -4,6 +4,17 @@
     <head>
         <meta http-equiv="Content-Type"   content="text/html; charset=utf-8" />
         <meta name="viewport"             content="width=device-width, initial-scale=1" />
+        <meta property="og:url"           content="http://rainbowsquad.xyz/" />
+        <meta property="og:type"          content="website" />
+        <meta property="og:title"         content="Taste the rainbow!" />
+        <meta property="og:description"   content="Taste the rainbow!" />
+        <meta property="og:image"         content="http://rainbowsquad.xyz/favicon.png" />
+        <meta name="url"                  content="http://rainbowsquad.xyz/" />
+        <meta name="type"                 content="website" />
+        <meta name="title"                content="Rainbow Squadron" />
+        <meta name="description"          content="Taste the rainbow!" />
+        <meta name="image"                content="http://rainbowsquad.xyz/favicon.png" />
+        <meta name="keywords"             content="rainbow, rbw, squad, squadron">
         <title>Rainbow Squadron</title>
         <script src="jquery.js"></script>
         <script src="jquery-ui.js"></script>
