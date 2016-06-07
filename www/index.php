dad7791239b5a3bb3f4bbe95930bb0083e8332f7
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-06-07 14:55:52 +0100

    setup json of us, need pictures and to write the d3

--- www/index.php
+++ www/index.php
@@ -8,12 +8,12 @@
         <meta property="og:type"          content="website" />
         <meta property="og:title"         content="Taste the rainbow!" />
         <meta property="og:description"   content="Taste the rainbow!" />
-        <meta property="og:image"         content="http://rainbowsquad.xyz/img/favicon.png" />
+        <meta property="og:image"         content="http://rainbowsquad.xyz/img/rbw.png" />
         <meta name="url"                  content="http://rainbowsquad.xyz/" />
         <meta name="type"                 content="website" />
         <meta name="title"                content="Rainbow Squadron" />
         <meta name="description"          content="Taste the rainbow!" />
-        <meta name="image"                content="http://rainbowsquad.xyz/img/favicon.png" />
+        <meta name="image"                content="http://rainbowsquad.xyz/img/rbw.png" />
         <meta name="keywords"             content="rainbow, rbw, squad, squadron">
         <title>Rainbow Squadron</title>
         <script src="js/jquery.js"></script>
@@ -21,7 +21,7 @@
         <script src="js/d3.js"></script>
         <script src="js/rbw.js"></script>
         <link type="text/css" rel="stylesheet" href="css/main.css"/>
-        <link rel="icon" href="img/favicon.png">
+        <link rel="icon" href="img/rbw.png">
     </head>
     <body onload="setup();">
         <div id="title">
